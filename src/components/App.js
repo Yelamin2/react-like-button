2ec898e8a685ab255fc93e1ed4ab3e7bce5ee0c8
yelamin2 <yelamin2@yahoo.com>
2022-09-27 12:20:06 -0400

    modifies homepage url

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,38 @@
 import React from 'react';
 import './App.css';
 import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
 
 
 function App(){
 
-  const LikeButton = () =>{
-    const [like, setLike] =useState(0),
-    onLikeButtonClick = () => {
-      setLike(like +1);
+  //work fro class
+  const [count, setCount] = useState(0);
+  return(
+    <>
+    <Button variant ="primary" onClick={() => setCount(count +1)}>{count} {count === 1 ? 'Like' : 'Likes'}</Button>
+    </>
+  );
+
+
+  //commented was my original work
+  // const LikeButton = () =>{
+  //   const [like, setLike] =useState(0),
+  //   onLikeButtonClick = () => {
+  //     setLike(like +1);
     
   
-    };
+  //   };
      
-     return (
-      <div className = "App">
-        <button onClick={onLikeButtonClick}>{like} {like === 1 ? 'Like': 'Likes'}</button>
+  //    return (
+  //     <div className = "App">
+  //       <Button variant="primary">Success</Button>
+  //       <button className="btn btn-primary" onClick={onLikeButtonClick}>{like} {like === 1 ? 'Like': 'Likes'}</button>
   
-      </div>
-    );
-  }
-  return LikeButton();
+  //     </div>
+  //   );
+  // }
+  // return LikeButton();
     
   
 
